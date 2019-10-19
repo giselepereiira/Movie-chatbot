a275e1a31a36b5199a86d4a489cf3fc56a6cd979
giselepereiira <gispereira22@gmail.com>
2019-10-19 20:31:57 +0100

    Added fallback policy

--- ConversationalAIMovieChatbot/Train-core-model.py
+++ ConversationalAIMovieChatbot/Train-core-model.py
@@ -1,4 +1,6 @@
 import logging
+
+from rasa.core.policies.fallback import FallbackPolicy
 from rasa.core.policies.keras_policy import KerasPolicy
 from rasa.core.policies.memoization import MemoizationPolicy
 from rasa.core.policies.mapping_policy import MappingPolicy
@@ -13,8 +15,9 @@ file_paths = GetFilePaths.getFilePaths()
 
 
 def train_core():
+    fallback_policy = FallbackPolicy(core_threshold=0.3, nlu_threshold=0.3) #TODO see this values
     agent = Agent(file_paths['movie_domain_path'],
-                  policies=[MemoizationPolicy(), KerasPolicy(), MappingPolicy(), FormPolicy()])
+                  policies=[MemoizationPolicy(), KerasPolicy(), MappingPolicy(), FormPolicy(), fallback_policy])
 
     loop = asyncio.get_event_loop()
     training_data = loop.run_until_complete(agent.load_data(file_paths['movie_training_files'] + 'stories.md'))
