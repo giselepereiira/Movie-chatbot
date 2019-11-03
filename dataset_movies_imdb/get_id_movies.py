d9df5e17c276ab02e662fc275f7874cfe088ab9c
giselepereiira <gispereira22@gmail.com>
2019-11-03 11:00:15 +0000

    Deleted unused functions

--- dataset_movies_imdb/get_id_movies.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def get_ids_movies():
-    list_ids = list()
-    with open('movies_id_list.txt') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            list_ids.append(row[0])
-    return list_ids
